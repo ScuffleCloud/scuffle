29ed1f072d6595e039d25cb619d776779dd0f6fc
Lucas Shanks <lucassshanks@gmail.com>
2025-03-08 03:36:44 -0500

    nits

--- website/src/components/left-nav/Navbar.svelte
+++ website/src/components/left-nav/Navbar.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import ScuffleLogo from '$lib/images/ScuffleLogo.svelte';
+    import NavLinks from './NavLinks.svelte';
     import OrganizationInfo from './OrganizationToggle.svelte';
 </script>
 
@@ -11,23 +12,15 @@
         scuffle
     </a>
     <OrganizationInfo />
-    <ul class="nav-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/dashboard">Dashboard</a></li>
-        <li><a href="/settings">Settings</a></li>
-    </ul>
+    <NavLinks />
 </nav>
 
 <style>
     .sidebar {
         width: 240px;
         height: 100vh;
-        background-color: var(--color-dark100);
-        color: var(--color-light100);
         display: flex;
         flex-direction: column;
-        box-shadow: 2px 0 5px rgba(0, 0, 0, 0.1);
         position: sticky;
         top: 0;
 
@@ -48,29 +41,6 @@
                 align-items: center;
                 justify-content: center;
                 filter: drop-shadow(0px 2px 4px 0px rgb(0, 0, 0, 0.05));
-            }
-        }
-
-        .nav-links {
-            list-style: none;
-            padding: 0;
-            margin: 0;
-
-            li {
-                margin-bottom: 0.5rem;
-
-                a {
-                    display: block;
-                    padding: 0.75rem 1rem;
-                    color: var(--color-light100);
-                    text-decoration: none;
-                    border-radius: 4px;
-                    transition: background-color 0.2s;
-
-                    &:hover {
-                        background-color: rgba(255, 255, 255, 0.1);
-                    }
-                }
             }
         }
     }

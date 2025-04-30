81c36ebe91857bd296fda4eae8803248b1896913
Lennart Kloock <lennart@scuffle.cloud>
2025-04-30 17:58:59 +0200

    push: help

---
+++ crates/isobmff/src/boxes/free.rs
@@ -0,0 +1,40 @@
+use crate::{BoxHeader, IsoBox};
+
+#[derive(IsoBox)]
+#[iso_box(box_type = b"free", crate_path = "crate")]
+pub struct FreeBox {
+    #[iso_box(header)]
+    pub header: BoxHeader,
+    #[iso_box(remaining)]
+    pub data: Vec<u8>,
+}
+
+// // expanded
+// impl<'a> IsoBox<'a> for FreeBox {
+//     const TYPE: [u8; 4] = *b"free";
+//     fn demux<R: ::scuffle_bytes_util::zero_copy::ZeroCopyReader<'a>>(
+//         header: crate::BoxHeader,
+//         mut payload_reader: R,
+//     ) -> ::std::io::Result<Self> {
+//         let data = {
+//             let mut remaining = Vec::new();
+//             loop {
+//                 let value = {
+//                     let data = match ::byteorder::ReadBytesExt::read_u8(
+//                         &mut ::scuffle_bytes_util::zero_copy::ZeroCopyReader::as_std(&mut payload_reader),
+//                     ) {
+//                         Ok(v) => v,
+//                         Err(e) if e.kind() == ::std::io::ErrorKind::UnexpectedEof => {
+//                             break;
+//                         }
+//                         Err(e) => return Err(e),
+//                     };
+//                     data
+//                 };
+//                 remaining.push(value);
+//             }
+//             remaining
+//         };
+//         Ok(Self { header: header, data })
+//     }
+// }

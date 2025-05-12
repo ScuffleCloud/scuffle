85ec18c892a494d3939eae71313158e175cf2517
Lennart Kloock <lennart@scuffle.cloud>
2025-05-12 22:20:40 +0200

    feat(isobmff): section 12 boxes

---
+++ crates/isobmff/src/boxes/haptic_media.rs
@@ -0,0 +1,26 @@
+use scuffle_bytes_util::zero_copy::Deserialize;
+
+use super::SampleEntry;
+
+/// Haptic sample entry
+///
+/// ISO/IEC 14496-12 - 12.11.3
+///
+/// Sub boxes:
+/// - [`btrt`](super::BitRateBox)
+/// - Any other boxes
+#[derive(Debug)]
+pub struct HapticSampleEntry {
+    pub sample_entry: SampleEntry,
+}
+
+impl<'a> Deserialize<'a> for HapticSampleEntry {
+    fn deserialize<R>(reader: R) -> std::io::Result<Self>
+    where
+        R: scuffle_bytes_util::zero_copy::ZeroCopyReader<'a>,
+    {
+        Ok(Self {
+            sample_entry: SampleEntry::deserialize(reader)?,
+        })
+    }
+}

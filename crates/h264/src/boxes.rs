711774282ab9d255d22585396e933e195e298b3d
Lennart Kloock <lennart@scuffle.cloud>
2025-05-13 01:14:07 +0200

    feat: update av1, h264 and h265

--- crates/h264/src/boxes.rs
+++ crates/h264/src/boxes.rs
@@ -1,177 +1,130 @@
-// TODO
-#![allow(missing_docs)]
+//! ISO base media file format boxes for AVC/h264.
+//!
+//! ISO/IEC 14496-15 - 5.4
 
-use isobmff::boxes::{
-    AmbientViewingEnvironmentBox, BitRateBox, CleanApertureBox, ColourInformationBox, ContentColourVolumeBox,
-    ContentLightLevelBox, MasteringDisplayColourVolumeBox, PixelAspectRatioBox, VisualSampleEntry,
-};
-use isobmff::{BoxHeader, IsoBox};
+use isobmff::boxes::{MPEG4ExtensionDescriptorsBox, VisualSampleEntry};
+use isobmff::{BoxHeader, IsoBox, UnknownBox};
 
 use crate::AVCDecoderConfigurationRecord;
 
+/// AVC configuration box
+///
+/// ISO/IEC 14496-15 - 5.4.2
 #[derive(IsoBox, Debug)]
 #[iso_box(box_type = b"avcC")]
 pub struct AVCConfigurationBox<'a> {
+    /// Header of the box.
     #[iso_box(header)]
     pub header: BoxHeader,
+    /// The AVC decoder configuration record.
     pub avc_config: AVCDecoderConfigurationRecord<'a>,
 }
 
-#[derive(IsoBox, Debug)]
-#[iso_box(box_type = b"m4ds")]
-pub struct MPEG4ExtensionDescriptorsBox {
-    #[iso_box(header)]
-    pub header: BoxHeader,
-    #[iso_box(repeated)]
-    pub descr: Vec<u8>,
-}
+// This doesn't make sense to me, the following 4 box types (avc1 - avc4) are all the same
 
-// This doesn't make sense to me, the following 4 boxe types (avc1 - avc4) are all the same
-
+/// AVC sample entry
+///
+/// ISO/IEC 14496-15 - 5.4.2
 #[derive(IsoBox, Debug)]
 #[iso_box(box_type = b"avc1")]
 pub struct AVCSampleEntry1<'a> {
+    /// Header of the box.
     #[iso_box(header)]
     pub header: BoxHeader,
+    /// The visual sample entry fields that this box inherits.
     pub visual_sample_entry: VisualSampleEntry,
-    #[iso_box(nested_box(collect))]
-    pub btrt: Option<BitRateBox>,
-    #[iso_box(nested_box(collect))]
-    pub clap: Option<CleanApertureBox>,
-    #[iso_box(nested_box(collect))]
-    pub pasp: Option<PixelAspectRatioBox>,
-    #[iso_box(nested_box(collect))]
-    pub colr: Option<ColourInformationBox<'a>>,
-    #[iso_box(nested_box(collect))]
-    pub clli: Option<ContentLightLevelBox>,
-    #[iso_box(nested_box(collect))]
-    pub mdcv: Option<MasteringDisplayColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub cclv: Option<ContentColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub amve: Option<AmbientViewingEnvironmentBox>,
+    /// The AVC configuration box contained in this box.
     #[iso_box(nested_box)]
     pub config: AVCConfigurationBox<'a>,
+    /// The optional MPEG-4 extension descriptors box contained in this box.
     #[iso_box(nested_box(collect))]
     pub mpeg4_extension: Option<MPEG4ExtensionDescriptorsBox>,
+    /// Any other boxes contained in this box.
     #[iso_box(nested_box(collect_unknown))]
-    pub unknown_boxes: Vec<isobmff::UnknownBox<'a>>,
+    pub sub_boxes: Vec<UnknownBox<'a>>,
 }
 
+/// AVC sample entry
+///
+/// ISO/IEC 14496-15 - 5.4.2
 #[derive(IsoBox, Debug)]
 #[iso_box(box_type = b"avc2")]
 pub struct AVCSampleEntry2<'a> {
+    /// Header of the box.
     #[iso_box(header)]
     pub header: BoxHeader,
+    /// The visual sample entry fields that this box inherits.
     pub visual_sample_entry: VisualSampleEntry,
-    #[iso_box(nested_box(collect))]
-    pub btrt: Option<BitRateBox>,
-    #[iso_box(nested_box(collect))]
-    pub clap: Option<CleanApertureBox>,
-    #[iso_box(nested_box(collect))]
-    pub pasp: Option<PixelAspectRatioBox>,
-    #[iso_box(nested_box(collect))]
-    pub colr: Option<ColourInformationBox<'a>>,
-    #[iso_box(nested_box(collect))]
-    pub clli: Option<ContentLightLevelBox>,
-    #[iso_box(nested_box(collect))]
-    pub mdcv: Option<MasteringDisplayColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub cclv: Option<ContentColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub amve: Option<AmbientViewingEnvironmentBox>,
+    /// The AVC configuration box contained in this box.
     #[iso_box(nested_box)]
     pub config: AVCConfigurationBox<'a>,
+    /// The optional MPEG-4 extension descriptors box contained in this box.
     #[iso_box(nested_box(collect))]
     pub mpeg4_extension: Option<MPEG4ExtensionDescriptorsBox>,
+    /// Any other boxes contained in this box.
     #[iso_box(nested_box(collect_unknown))]
-    pub unknown_boxes: Vec<isobmff::UnknownBox<'a>>,
+    pub sub_boxes: Vec<UnknownBox<'a>>,
 }
 
+/// AVC sample entry
+///
+/// ISO/IEC 14496-15 - 5.4.2
 #[derive(IsoBox, Debug)]
 #[iso_box(box_type = b"avc3")]
 pub struct AVCSampleEntry3<'a> {
+    /// Header of the box.
     #[iso_box(header)]
     pub header: BoxHeader,
+    /// The visual sample entry fields that this box inherits.
     pub visual_sample_entry: VisualSampleEntry,
-    #[iso_box(nested_box(collect))]
-    pub btrt: Option<BitRateBox>,
-    #[iso_box(nested_box(collect))]
-    pub clap: Option<CleanApertureBox>,
-    #[iso_box(nested_box(collect))]
-    pub pasp: Option<PixelAspectRatioBox>,
-    #[iso_box(nested_box(collect))]
-    pub colr: Option<ColourInformationBox<'a>>,
-    #[iso_box(nested_box(collect))]
-    pub clli: Option<ContentLightLevelBox>,
-    #[iso_box(nested_box(collect))]
-    pub mdcv: Option<MasteringDisplayColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub cclv: Option<ContentColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub amve: Option<AmbientViewingEnvironmentBox>,
+    /// The AVC configuration box contained in this box.
     #[iso_box(nested_box)]
     pub config: AVCConfigurationBox<'a>,
+    /// The optional MPEG-4 extension descriptors box contained in this box.
     #[iso_box(nested_box(collect))]
     pub mpeg4_extension: Option<MPEG4ExtensionDescriptorsBox>,
+    /// Any other boxes contained in this box.
     #[iso_box(nested_box(collect_unknown))]
-    pub unknown_boxes: Vec<isobmff::UnknownBox<'a>>,
+    pub sub_boxes: Vec<UnknownBox<'a>>,
 }
 
+/// AVC sample entry
+///
+/// ISO/IEC 14496-15 - 5.4.2
 #[derive(IsoBox, Debug)]
 #[iso_box(box_type = b"avc4")]
 pub struct AVCSampleEntry4<'a> {
+    /// Header of the box.
     #[iso_box(header)]
     pub header: BoxHeader,
+    /// The visual sample entry fields that this box inherits.
     pub visual_sample_entry: VisualSampleEntry,
-    #[iso_box(nested_box(collect))]
-    pub btrt: Option<BitRateBox>,
-    #[iso_box(nested_box(collect))]
-    pub clap: Option<CleanApertureBox>,
-    #[iso_box(nested_box(collect))]
-    pub pasp: Option<PixelAspectRatioBox>,
-    #[iso_box(nested_box(collect))]
-    pub colr: Option<ColourInformationBox<'a>>,
-    #[iso_box(nested_box(collect))]
-    pub clli: Option<ContentLightLevelBox>,
-    #[iso_box(nested_box(collect))]
-    pub mdcv: Option<MasteringDisplayColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub cclv: Option<ContentColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub amve: Option<AmbientViewingEnvironmentBox>,
+    /// The AVC configuration box contained in this box.
     #[iso_box(nested_box)]
     pub config: AVCConfigurationBox<'a>,
+    /// The optional MPEG-4 extension descriptors box contained in this box.
     #[iso_box(nested_box(collect))]
     pub mpeg4_extension: Option<MPEG4ExtensionDescriptorsBox>,
+    /// Any other boxes contained in this box.
     #[iso_box(nested_box(collect_unknown))]
-    pub unknown_boxes: Vec<isobmff::UnknownBox<'a>>,
+    pub sub_boxes: Vec<UnknownBox<'a>>,
 }
 
+/// AVC parameter sample entry
+///
+/// ISO/IEC 14496-15 - 5.4.3
 #[derive(IsoBox, Debug)]
 #[iso_box(box_type = b"avcp")]
 pub struct AVCParameterSampleEntry<'a> {
+    /// Header of the box.
     #[iso_box(header)]
     pub header: BoxHeader,
+    /// The visual sample entry fields that this box inherits.
     pub visual_sample_entry: VisualSampleEntry,
-    #[iso_box(nested_box(collect))]
-    pub btrt: Option<BitRateBox>,
-    #[iso_box(nested_box(collect))]
-    pub clap: Option<CleanApertureBox>,
-    #[iso_box(nested_box(collect))]
-    pub pasp: Option<PixelAspectRatioBox>,
-    #[iso_box(nested_box(collect))]
-    pub colr: Option<ColourInformationBox<'a>>,
-    #[iso_box(nested_box(collect))]
-    pub clli: Option<ContentLightLevelBox>,
-    #[iso_box(nested_box(collect))]
-    pub mdcv: Option<MasteringDisplayColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub cclv: Option<ContentColourVolumeBox>,
-    #[iso_box(nested_box(collect))]
-    pub amve: Option<AmbientViewingEnvironmentBox>,
+    /// The AVC configuration box contained in this box.
     #[iso_box(nested_box)]
     pub config: AVCConfigurationBox<'a>,
+    /// Any other boxes contained in this box.
     #[iso_box(nested_box(collect_unknown))]
-    pub unknown_boxes: Vec<isobmff::UnknownBox<'a>>,
+    pub sub_boxes: Vec<UnknownBox<'a>>,
 }

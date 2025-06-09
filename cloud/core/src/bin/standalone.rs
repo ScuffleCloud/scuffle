f4996f316f078d5a2f1c4c2d8933e40020a987e9
Lennart Kloock <lennart@scuffle.cloud>
2025-06-09 21:20:03 +0200

    chore(core): fmt and readme

--- cloud/core/src/bin/standalone.rs
+++ cloud/core/src/bin/standalone.rs
@@ -1,11 +1,3 @@
-//! Core/Authentication server for <https://scuffle.cloud/>.
-//!
-//! ## License
-//!
-//! This project is licensed under the [AGPL-3.0](./LICENSE.AGPL-3.0).
-//!
-//! `SPDX-License-Identifier: AGPL-3.0`
-
 use std::net::SocketAddr;
 use std::sync::Arc;
 

d8d62ff209d5646952ce64b3d959ba5c7c788cb3
Lennart Kloock <lennart@scuffle.cloud>
2025-06-09 11:37:27 +0200

    feat(core): setup core binary

---
+++ cloud/core/src/service.rs
@@ -0,0 +1,40 @@
+use std::sync::Arc;
+
+use tinc::TincService;
+
+use crate::Global;
+
+pub struct CoreSvc;
+
+impl scuffle_bootstrap::Service<Global> for CoreSvc {
+    async fn run(self, global: Arc<Global>, ctx: scuffle_context::Context) -> anyhow::Result<()> {
+        let tinc = pb::scufflecloud::core::v1::core_service_tinc::CoreServiceTinc::new(CoreSvc);
+
+        scuffle_http::HttpServer::builder()
+            .tower_make_service_factory(tinc.into_router().into_make_service())
+            .bind(global.config.bind)
+            .ctx(ctx)
+            .build()
+            .run()
+            .await?;
+
+        Ok(())
+    }
+}
+
+#[async_trait::async_trait]
+impl pb::scufflecloud::core::v1::core_service_server::CoreService for CoreSvc {
+    async fn register_with_email(
+        &self,
+        _request: tonic::Request<pb::scufflecloud::core::v1::RegisterWithEmailRequest>,
+    ) -> Result<tonic::Response<pb::scufflecloud::core::v1::SessionToken>, tonic::Status> {
+        Err(tonic::Status::unimplemented("Not implemented yet"))
+    }
+
+    async fn register_with_external_provider(
+        &self,
+        _request: tonic::Request<pb::scufflecloud::core::v1::RegisterWithExternalProviderRequest>,
+    ) -> Result<tonic::Response<pb::scufflecloud::core::v1::RegisterWithExternalProviderResponse>, tonic::Status> {
+        Err(tonic::Status::unimplemented("Not implemented yet"))
+    }
+}

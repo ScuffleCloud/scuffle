270b6a14d33f00e9c9c5e8dcb391f6ec943d9fa2
Lucas Shanks <lucassshanks@gmail.com>
2025-06-23 17:53:51 -0400

    create asset tab

--- website/src/lib/images/icon-stream.svelte
+++ website/src/lib/images/icon-stream.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    let { size = 1.2 * 16 }: { size?: number } = $props();
+    let { size = 16 }: { size?: number } = $props();
 </script>
 
 <svg width={size} height={size} viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">

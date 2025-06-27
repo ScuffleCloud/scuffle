2aac363a4b551ef6ddeb6068a2fb1162848a8465
Lennart Kloock <lennart@scuffle.cloud>
2025-06-27 11:29:52 +0200

    feat(core): complete email flow

--- cloud/core/src/services/organizations.rs
+++ cloud/core/src/services/organizations.rs
@@ -1,3 +1,5 @@
+use tonic_types::{ErrorDetails, StatusExt};
+
 use crate::CoreConfig;
 use crate::services::CoreSvc;
 
@@ -5,29 +7,45 @@ use crate::services::CoreSvc;
 impl<G: CoreConfig> pb::scufflecloud::core::v1::organizations_service_server::OrganizationsService for CoreSvc<G> {
     async fn create_organization(
         &self,
-        _request: tonic::Request<pb::scufflecloud::core::v1::CreateOrganizationRequest>,
+        _req: tonic::Request<pb::scufflecloud::core::v1::CreateOrganizationRequest>,
     ) -> Result<tonic::Response<pb::scufflecloud::core::v1::Organization>, tonic::Status> {
-        Err(tonic::Status::unimplemented("not implemented yet"))
+        Err(tonic::Status::with_error_details(
+            tonic::Code::Unimplemented,
+            "this endpoint is not implemented",
+            ErrorDetails::new(),
+        ))
     }
 
     async fn get_organization(
         &self,
-        _request: tonic::Request<pb::scufflecloud::core::v1::OrganizationByIdRequest>,
+        _req: tonic::Request<pb::scufflecloud::core::v1::OrganizationByIdRequest>,
     ) -> Result<tonic::Response<pb::scufflecloud::core::v1::Organization>, tonic::Status> {
-        Err(tonic::Status::unimplemented("not implemented yet"))
+        Err(tonic::Status::with_error_details(
+            tonic::Code::Unimplemented,
+            "this endpoint is not implemented",
+            ErrorDetails::new(),
+        ))
     }
 
     async fn list_organization_members(
         &self,
-        _request: tonic::Request<pb::scufflecloud::core::v1::OrganizationByIdRequest>,
+        _req: tonic::Request<pb::scufflecloud::core::v1::OrganizationByIdRequest>,
     ) -> Result<tonic::Response<pb::scufflecloud::core::v1::OrganizationMemberList>, tonic::Status> {
-        Err(tonic::Status::unimplemented("not implemented yet"))
+        Err(tonic::Status::with_error_details(
+            tonic::Code::Unimplemented,
+            "this endpoint is not implemented",
+            ErrorDetails::new(),
+        ))
     }
 
     async fn list_organizations_by_user(
         &self,
-        _request: tonic::Request<pb::scufflecloud::core::v1::UserByIdRequest>,
+        _req: tonic::Request<pb::scufflecloud::core::v1::UserByIdRequest>,
     ) -> Result<tonic::Response<pb::scufflecloud::core::v1::OrganizationList>, tonic::Status> {
-        Err(tonic::Status::unimplemented("not implemented yet"))
+        Err(tonic::Status::with_error_details(
+            tonic::Code::Unimplemented,
+            "this endpoint is not implemented",
+            ErrorDetails::new(),
+        ))
     }
 }
---
+++ cloud/core/src/session_crypto.rs
@@ -0,0 +1,26 @@
+use pb::scufflecloud::core::v1::DeviceAlgorithm;
+use rand::TryRngCore;
+use rsa::pkcs8::DecodePublicKey;
+use tonic::Code;
+
+use crate::result_ext::ResultExt;
+
+pub(crate) fn generate_token() -> Result<[u8; 32], rand::rand_core::OsError> {
+    let mut token = [0u8; 32];
+    rand::rngs::OsRng.try_fill_bytes(&mut token)?;
+    Ok(token)
+}
+
+pub(crate) fn encrypt_token(algorithm: DeviceAlgorithm, token: &[u8], pk_der_data: &[u8]) -> Result<Vec<u8>, tonic::Status> {
+    match algorithm {
+        DeviceAlgorithm::RsaOaepSha256 => {
+            let pk = rsa::RsaPublicKey::from_public_key_der(pk_der_data)
+                .into_tonic(Code::InvalidArgument, "failed to parse public key")?;
+            let padding = rsa::Oaep::new::<sha2::Sha256>();
+            let enc_data = pk
+                .encrypt(&mut rsa::rand_core::OsRng, padding, token)
+                .into_tonic_internal("failed to encrypt token")?;
+            Ok(enc_data)
+        }
+    }
+}

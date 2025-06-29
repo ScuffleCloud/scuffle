646c609df403c0523f881960feea774f405ade41
Lennart Kloock <lennart@scuffle.cloud>
2025-06-29 22:08:21 +0200

    feat(core): login_with_webauthn_public_key and create_user_session_request

--- cloud/core/src/services/sessions/mod.rs
+++ cloud/core/src/services/sessions/mod.rs
@@ -8,8 +8,12 @@ use tonic_types::{ErrorDetails, StatusExt};
 
 use crate::http_ext::RequestExt;
 use crate::id::Id;
-use crate::models::{EmailRegistrationRequest, EmailRegistrationRequestId, MagicLinkUserSessionRequest, UserEmail};
-use crate::schema::{email_registration_requests, magic_link_user_session_requests, user_emails};
+use crate::models::{
+    EmailRegistrationRequest, EmailRegistrationRequestId, MagicLinkUserSessionRequest, UserEmail, UserSessionRequest,
+};
+use crate::schema::{
+    email_registration_requests, magic_link_user_session_requests, mfa_webauthn_pks, user_emails, user_session_requests,
+};
 use crate::services::CoreSvc;
 use crate::std_ext::{OptionExt, ResultExt};
 use crate::utils::TxError;
@@ -59,7 +63,7 @@ impl<G: CoreConfig> pb::scufflecloud::core::v1::sessions_service_server::Session
         }
 
         // Generate random code
-        let code = crypto::generate_token().into_tonic_internal("failed to generate registration code")?;
+        let code = crypto::generate_random_bytes().into_tonic_internal("failed to generate registration code")?;
 
         // Create email registration request
         let registration_request = EmailRegistrationRequest {
@@ -252,7 +256,7 @@ impl<G: CoreConfig> pb::scufflecloud::core::v1::sessions_service_server::Session
             async move {
                 let user = common::get_user_by_email(conn, &payload.email).await?;
 
-                let code = crypto::generate_token().into_tonic_internal("failed to generate magic link code")?;
+                let code = crypto::generate_random_bytes().into_tonic_internal("failed to generate magic link code")?;
 
                 // Insert email link user session request
                 let session_request = MagicLinkUserSessionRequest {
@@ -344,12 +348,55 @@ impl<G: CoreConfig> pb::scufflecloud::core::v1::sessions_service_server::Session
 
     async fn login_with_webauthn_public_key(
         &self,
-        _req: tonic::Request<pb::scufflecloud::core::v1::LoginWithWebauthnPublicKeyRequest>,
+        req: tonic::Request<pb::scufflecloud::core::v1::LoginWithWebauthnPublicKeyRequest>,
     ) -> Result<tonic::Response<pb::scufflecloud::core::v1::LoginWithWebauthnPublicKeyResponse>, tonic::Status> {
-        Err(tonic::Status::with_error_details(
-            tonic::Code::Unimplemented,
-            "this endpoint is not implemented",
-            ErrorDetails::new(),
+        let (_, extensions, payload) = req.into_parts();
+        let global = extensions.global::<G>()?;
+
+        let mut db = global.db().await.into_tonic_internal("failed to connect to database")?;
+
+        // Check captcha
+        let captcha = payload.captcha.require("captcha")?;
+        match captcha.provider() {
+            pb::scufflecloud::core::v1::CaptchaProvider::Turnstile => {
+                captcha::turnstile::verify_in_tonic(global, &captcha.token).await?;
+            }
+        }
+
+        let challenge = db
+            .transaction::<_, TxError, _>(|conn| {
+                async move {
+                    let challenge =
+                        crypto::generate_random_bytes().into_tonic_internal("failed to generate webauthn challenge")?;
+
+                    let n = diesel::update(mfa_webauthn_pks::dsl::mfa_webauthn_pks)
+                        .filter(mfa_webauthn_pks::dsl::pk_id.eq(&payload.pk_id))
+                        .set((
+                            mfa_webauthn_pks::dsl::current_challenge.eq(&challenge),
+                            mfa_webauthn_pks::dsl::current_challenge_expires_at
+                                .eq(chrono::Utc::now() + chrono::Duration::minutes(5)),
+                        ))
+                        .execute(conn)
+                        .await
+                        .into_tonic_internal("failed to update webauthn public key")?;
+
+                    if n == 0 {
+                        return Err(tonic::Status::with_error_details(
+                            tonic::Code::NotFound,
+                            "webauthn public key not found",
+                            ErrorDetails::new(),
+                        )
+                        .into());
+                    }
+
+                    Ok(challenge.to_vec())
+                }
+                .scope_boxed()
+            })
+            .await?;
+
+        Ok(tonic::Response::new(
+            pb::scufflecloud::core::v1::LoginWithWebauthnPublicKeyResponse { challenge },
         ))
     }
 
@@ -366,13 +413,32 @@ impl<G: CoreConfig> pb::scufflecloud::core::v1::sessions_service_server::Session
 
     async fn create_user_session_request(
         &self,
-        _req: tonic::Request<pb::scufflecloud::core::v1::CreateUserSessionRequestRequest>,
+        req: tonic::Request<pb::scufflecloud::core::v1::CreateUserSessionRequestRequest>,
     ) -> Result<tonic::Response<pb::scufflecloud::core::v1::UserSessionRequest>, tonic::Status> {
-        Err(tonic::Status::with_error_details(
-            tonic::Code::Unimplemented,
-            "this endpoint is not implemented",
-            ErrorDetails::new(),
-        ))
+        let (_, extensions, payload) = req.into_parts();
+        let global = extensions.global::<G>()?;
+
+        let ip_info = extensions.ip_address_info()?;
+        let mut db = global.db().await.into_tonic_internal("failed to connect to database")?;
+
+        let code = format!("{:06}", rand::random_range(0..=999999));
+
+        let session_request = UserSessionRequest {
+            id: Id::new(),
+            device_name: payload.name,
+            device_ip: ip_info.to_network(),
+            code,
+            approved_by: None,
+            expires_at: chrono::Utc::now() + chrono::Duration::minutes(5),
+        };
+
+        diesel::insert_into(user_session_requests::dsl::user_session_requests)
+            .values(&session_request)
+            .execute(&mut *db)
+            .await
+            .into_tonic_internal("failed to insert user session request")?;
+
+        Ok(tonic::Response::new(session_request.into()))
     }
 
     async fn get_user_session_request(

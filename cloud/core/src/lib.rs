ebf338d0be718818e2e759904494eba5e1bdcbef
Lennart Kloock <lennart@scuffle.cloud>
2025-07-10 11:22:07 +0200

    fix(core): attribute and readme

--- cloud/core/src/lib.rs
+++ cloud/core/src/lib.rs
@@ -9,7 +9,7 @@
 //! This project is licensed under the [AGPL-3.0](./LICENSE.AGPL-3.0).
 //!
 //! `SPDX-License-Identifier: AGPL-3.0`
-#![cfg_attr(all(coverage_nightly, test), feature(coverage_attribute))]
+#![cfg_attr(coverage_nightly, feature(coverage_attribute))]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 // #![deny(missing_docs)]
 #![deny(unsafe_code)]

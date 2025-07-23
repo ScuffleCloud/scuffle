50a5f415539b971640192f0781ce77e7f0e2b66c
Lennart Kloock <lennart@scuffle.cloud>
2025-07-23 21:12:41 +0200

    fix(core): smaller webauthn challenges

--- cloud/core/src/services/mod.rs
+++ cloud/core/src/services/mod.rs
@@ -32,37 +32,57 @@ impl<G> Default for CoreSvc<G> {
 impl<G: CoreConfig> scuffle_bootstrap::Service<G> for CoreSvc<G> {
     async fn run(self, global: Arc<G>, ctx: scuffle_context::Context) -> anyhow::Result<()> {
         // REST
+        let organization_invitations_svc_tinc =
+            pb::scufflecloud::core::v1::organization_invitations_service_tinc::OrganizationInvitationsServiceTinc::new(
+                CoreSvc::<G>::default(),
+            );
         let organizations_svc_tinc =
             pb::scufflecloud::core::v1::organizations_service_tinc::OrganizationsServiceTinc::new(CoreSvc::<G>::default());
         let sessions_svc_tinc =
             pb::scufflecloud::core::v1::sessions_service_tinc::SessionsServiceTinc::new(CoreSvc::<G>::default());
         let users_svc_tinc = pb::scufflecloud::core::v1::users_service_tinc::UsersServiceTinc::new(CoreSvc::<G>::default());
+        let webauthn_challenges_svc_tinc =
+            pb::scufflecloud::core::v1::webauthn_challenges_service_tinc::WebauthnChallengesServiceTinc::new(
+                CoreSvc::<G>::default(),
+            );
 
         let cors = CorsLayer::new()
             .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
             .allow_origin(tower_http::cors::Any)
             .allow_headers(tower_http::cors::Any);
 
-        let mut openapi_schema = organizations_svc_tinc.openapi_schema();
+        let mut openapi_schema = organization_invitations_svc_tinc.openapi_schema();
+        openapi_schema.merge(organizations_svc_tinc.openapi_schema());
         openapi_schema.merge(sessions_svc_tinc.openapi_schema());
         openapi_schema.merge(users_svc_tinc.openapi_schema());
+        openapi_schema.merge(webauthn_challenges_svc_tinc.openapi_schema());
         openapi_schema.info.title = "Scuffle Cloud Core API".to_string();
         openapi_schema.info.version = "v1".to_string();
 
         let v1_rest_router = axum::Router::new()
             .route("/openapi.json", axum::routing::get(Json(openapi_schema)))
+            .merge(organization_invitations_svc_tinc.into_router())
             .merge(organizations_svc_tinc.into_router())
             .merge(sessions_svc_tinc.into_router())
             .merge(users_svc_tinc.into_router())
+            .merge(webauthn_challenges_svc_tinc.into_router())
             .layer(cors);
 
         // gRPC
+        let organization_invitations_svc =
+            pb::scufflecloud::core::v1::organization_invitations_service_server::OrganizationInvitationsServiceServer::new(
+                CoreSvc::<G>::default(),
+            );
         let organizations_svc = pb::scufflecloud::core::v1::organizations_service_server::OrganizationsServiceServer::new(
             CoreSvc::<G>::default(),
         );
         let sessions_svc =
             pb::scufflecloud::core::v1::sessions_service_server::SessionsServiceServer::new(CoreSvc::<G>::default());
         let users_svc = pb::scufflecloud::core::v1::users_service_server::UsersServiceServer::new(CoreSvc::<G>::default());
+        let webauthn_challenges_svc =
+            pb::scufflecloud::core::v1::webauthn_challenges_service_server::WebauthnChallengesServiceServer::new(
+                CoreSvc::<G>::default(),
+            );
 
         let reflection_v1_svc = tonic_reflection::server::Builder::configure()
             .register_encoded_file_descriptor_set(pb::ANNOTATIONS_PB)
@@ -72,9 +92,11 @@ impl<G: CoreConfig> scuffle_bootstrap::Service<G> for CoreSvc<G> {
             .build_v1alpha()?;
 
         let mut builder = tonic::service::Routes::builder();
+        builder.add_service(organization_invitations_svc);
         builder.add_service(organizations_svc);
         builder.add_service(sessions_svc);
         builder.add_service(users_svc);
+        builder.add_service(webauthn_challenges_svc);
         builder.add_service(reflection_v1_svc);
         builder.add_service(reflection_v1alpha_svc);
         let grpc_router = builder.routes().prepare().into_axum_router();

290cdc5860e4910df09b3ebf8127b23b138decab
Lennart Kloock <lennart@scuffle.cloud>
2025-09-08 22:58:10 +0200

    fix(email): remove rest routes

--- cloud/email/src/services/mod.rs
+++ cloud/email/src/services/mod.rs
@@ -1,12 +1,8 @@
 use std::net::SocketAddr;
 use std::sync::Arc;
 
+use axum::Extension;
 use axum::http::StatusCode;
-use axum::{Extension, Json};
-use scuffle_http::http::Method;
-use tinc::TincService;
-use tinc::openapi::Server;
-use tower_http::cors::CorsLayer;
 use tower_http::trace::TraceLayer;
 
 use crate::EmailConfig;
@@ -26,29 +22,8 @@ impl<G> Default for EmailSvc<G> {
     }
 }
 
-fn rest_cors_layer() -> CorsLayer {
-    CorsLayer::new()
-        .allow_methods([Method::GET, Method::POST, Method::OPTIONS])
-        .allow_origin(tower_http::cors::Any)
-        .allow_headers(tower_http::cors::Any)
-}
-
 impl<G: EmailConfig> scuffle_bootstrap::Service<G> for EmailSvc<G> {
     async fn run(self, global: Arc<G>, ctx: scuffle_context::Context) -> anyhow::Result<()> {
-        // REST
-        let email_svc_tinc =
-            pb::scufflecloud::email::v1::email_service_tinc::EmailServiceTinc::new(EmailSvc::<G>::default());
-
-        let mut openapi_schema = email_svc_tinc.openapi_schema();
-        openapi_schema.info.title = "Scuffle Cloud Mail API".to_string();
-        openapi_schema.info.version = "v1".to_string();
-        openapi_schema.servers = Some(vec![Server::new("/v1")]);
-
-        let v1_rest_router = axum::Router::new()
-            .route("/openapi.json", axum::routing::get(Json(openapi_schema)))
-            .merge(email_svc_tinc.into_router())
-            .layer(rest_cors_layer());
-
         // gRPC
         let email_svc = pb::scufflecloud::email::v1::email_service_server::EmailServiceServer::new(EmailSvc::<G>::default());
 
@@ -66,7 +41,6 @@ impl<G: EmailConfig> scuffle_bootstrap::Service<G> for EmailSvc<G> {
         let grpc_router = builder.routes().prepare().into_axum_router();
 
         let router = axum::Router::new()
-            .nest("/v1", v1_rest_router)
             .merge(grpc_router)
             .layer(TraceLayer::new_for_http())
             .layer(Extension(Arc::clone(&global)))

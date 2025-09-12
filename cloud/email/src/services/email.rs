f6cb4d3db5b9fb4ce88096dbc05762d2c0f4c68a
Lennart Kloock <lennart@scuffle.cloud>
2025-09-12 23:25:39 +0200

    feat(core): update emails

--- cloud/email/src/services/email.rs
+++ cloud/email/src/services/email.rs
@@ -1,6 +1,3 @@
-use tonic::Code;
-use tonic_types::{ErrorDetails, StatusExt};
-
 use crate::EmailConfig;
 use crate::services::EmailSvc;
 
@@ -10,10 +7,6 @@ impl<G: EmailConfig> pb::scufflecloud::email::v1::email_service_server::EmailSer
         &self,
         _req: tonic::Request<pb::scufflecloud::email::v1::Email>,
     ) -> Result<tonic::Response<()>, tonic::Status> {
-        Err(tonic::Status::with_error_details(
-            Code::Unimplemented,
-            "this call is not implemented yet",
-            ErrorDetails::new(),
-        ))
+        Ok(tonic::Response::new(()))
     }
 }

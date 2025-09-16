d794eab00193598c5b71084a6f0aeade9d3fbac3
Lucas Shanks <lucassshanks@gmail.com>
2025-09-16 17:50:05 +0800

    nits

--- cloud/dashboard/src/routes/+page.ts
+++ cloud/dashboard/src/routes/+page.ts
@@ -1,17 +1,12 @@
-// import { useAuth } from "$lib/auth.svelte";
-// import { redirect } from "@sveltejs/kit";
+import { useAuth } from "$lib/auth.svelte";
+import { redirect } from "@sveltejs/kit";
 
-// export function load() {
-//     const auth = useAuth();
+export function load() {
+    const auth = useAuth();
 
-//     // If authenticated, redirect to the dashboard
-//     if (auth.userSessionToken.state === "authenticated") {
-//         throw redirect(307, "/settings/user/common");
-//     }
+    if (auth.userSessionToken.state === "authenticated") {
+        throw redirect(307, "/settings/user/common");
+    }
 
-//     if (auth.userSessionToken.state === "unauthenticated") {
-//         throw redirect(307, "/login");
-//     }
-
-//     return {};
-// }
+    throw redirect(307, "/login");
+}

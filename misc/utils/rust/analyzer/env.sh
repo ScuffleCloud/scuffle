c090f4c8bfed768844616e315b66af28b077539d
Troy Benson <troybensonsa@gmail.com>
2025-10-10 09:30:41 -0400

    reformat and fix gha

--- misc/utils/rust/analyzer/env.sh
+++ misc/utils/rust/analyzer/env.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 
 _bazel__get_workspace_path() {
-  local workspace=$PWD
-  while true; do
-    if [ -f "${workspace}/WORKSPACE" ] || \
-       [ -f "${workspace}/WORKSPACE.bazel" ] || \
-       [ -f "${workspace}/MODULE.bazel" ] || \
-       [ -f "${workspace}/REPO.bazel" ]; then
-      break
-    elif [ -z "$workspace" ] || [ "$workspace" = "/" ]; then
-      workspace=$PWD
-      break;
-    fi
-    workspace=${workspace%/*}
-  done
-  echo "$workspace"
+    local workspace=$PWD
+    while true; do
+        if [ -f "${workspace}/WORKSPACE" ] \
+            || [ -f "${workspace}/WORKSPACE.bazel" ] \
+            || [ -f "${workspace}/MODULE.bazel" ] \
+            || [ -f "${workspace}/REPO.bazel" ]; then
+            break
+        elif [ -z "$workspace" ] || [ "$workspace" = "/" ]; then
+            workspace=$PWD
+            break
+        fi
+        workspace=${workspace%/*}
+    done
+    echo "$workspace"
 }
 
 root_dir="$(_bazel__get_workspace_path)"
 bazel_env_bin_dir="${root_dir}/target-bazel/out/bazel_env-opt-ST-6691f78f59fa/bin/bazel_env/bin"
 
-if [[ ! -d "${bazel_env_bin_dir}" ]]; then
+if [[ ! -d ${bazel_env_bin_dir} ]]; then
     echo "bazel_env_bin_dir does not exist: ${bazel_env_bin_dir}"
     exit 1
 fi
